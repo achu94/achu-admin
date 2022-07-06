a732a09f348d1925824997a20d628c06fa98abbd
aygyun94 <aygyun_94@outlook.de>
2022-07-07 00:39:14 +0200

    Navbar...

---
+++ client/src/components/Layout/Navbar/Navbar.js
@@ -0,0 +1,121 @@
+import {useState} from "react";
+import {createMedia} from "@artsy/fresnel";
+import {Container, Icon, Image, Menu, Sidebar} from "semantic-ui-react";
+
+const AppMedia = createMedia({
+    breakpoints: {
+        mobile: 320,
+        tablet: 768,
+        computer: 992,
+        largeScreen: 1200,
+        widescreen: 1920
+    }
+});
+
+const {Media} = AppMedia;
+
+const NavBarMobile = (props) => {
+    const {
+        children,
+        leftItems,
+        onPusherClick,
+        onToggle,
+        rightItems,
+        visible
+    } = props;
+
+    return (
+        <Sidebar.Pushable>
+            <Sidebar
+                as={Menu}
+                animation="overlay"
+                icon="labeled"
+                inverted
+                items={leftItems}
+                vertical
+                visible={visible}
+            />
+            <Sidebar.Pusher
+                dimmed={visible}
+                onClick={onPusherClick}
+                style={{minHeight: "100vh"}}
+            >
+                <Menu fixed="top" inverted>
+                    <Menu.Item>
+                        <Image size="mini" src="https://react.semantic-ui.com/logo.png"/>
+                    </Menu.Item>
+                    <Menu.Item onClick={onToggle}>
+                        <Icon name="sidebar"/>
+                    </Menu.Item>
+                    <Menu.Menu position="right">
+                        {rightItems.map((item) => {
+                            <Menu.Item {...item}/>
+                        })}
+                    </Menu.Menu>
+                </Menu>
+                {children}
+            </Sidebar.Pusher>
+        </Sidebar.Pushable>
+    );
+}
+
+const NavBarDesktop = (props) => {
+    const {leftItems, rightItems} = props;
+
+    return (
+        <Menu fixed="top" inverted>
+            <Menu.Item>
+                <Image size="mini" src="https://react.semantic-ui.com/logo.png"/>
+            </Menu.Item>
+
+            {leftItems.map((item) => (
+                <Menu.Item {...item} />
+            ))}
+
+            <Menu.Menu position="right">
+                {rightItems.map((item) => (
+                    <Menu.Item {...item} />
+                ))}
+            </Menu.Menu>
+        </Menu>
+    );
+};
+
+const NavBarChildren = (props) => (
+    <Container style={{marginTop: "5em"}}>{props.children}</Container>
+);
+
+const Navbar = (props) => {
+    const [visible, setVisible] = useState(false);
+
+    const handlePusher = () => {
+        if (visible) setVisible(false);
+    }
+
+    const handleToggle = () => setVisible(!visible);
+
+    const {children, leftItems, rightItems} = props;
+
+    return (
+        <div>
+            <Media at="mobile">
+                <NavBarMobile
+                    leftItems={leftItems}
+                    onPusherClick={handlePusher}
+                    onToggle={handleToggle}
+                    rightItems={rightItems}
+                    visible={visible}
+                >
+                    <NavBarChildren>{children}</NavBarChildren>
+                </NavBarMobile>
+            </Media>
+
+            <Media greaterThan="mobile">
+                <NavBarDesktop leftItems={leftItems} rightItems={rightItems}/>
+                <NavBarChildren>{children}</NavBarChildren>
+            </Media>
+        </div>
+    );
+}
+
+export default Navbar;
